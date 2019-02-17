3fd0a5ff8cab353a45696bc36df5d83ceab09ea4
sola <solaocodes@gmail.com>
2019-02-17 12:02:00 -0500

    Pass through fake placeholder data for writings page.

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("landing"));
 
-app.get("/writings", (req, res) => res.render("writings"));
+app.get("/writings", (req, res) => {
+  let writings = [
+    {title: "On Wealth", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    {title: "Sunday Morning for Sinners", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    {title: "Happy", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+  ];
+
+  res.render("writings", {writings: writings});
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
