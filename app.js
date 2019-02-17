2e8dd334ee8182d48fc29ac8700914f05b21d3af
sola <solaocodes@gmail.com>
2019-02-17 11:54:20 -0500

    Add a route for the writings page.

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("landing"));
 
+app.get("/writings", (req, res) => res.render("writings"));
+
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
