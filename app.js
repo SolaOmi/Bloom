44bcb227b60dc0331822c03c506e8e4a7e7a0e77
sola <solaocodes@gmail.com>
2019-02-20 23:27:55 -0500

    Remove fake data and replace with sample data from development mongoDB.

--- app.js
+++ app.js
@@ -13,16 +13,36 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let writings = [
-  {title: "On Wealth", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-  {title: "Sunday Morning for Sinners", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-  {title: "Happy", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-];
+let writingSchema = new mongoose.Schema({
+  title: String,
+  body: String
+});
+
+let Writing = mongoose.model("Writing", writingSchema);
+
+// Writing.create(
+//   {
+//     title: "Five",
+//     body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+//   }, (err, writing) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log("NEWLY CREATED WRITING: ");
+//           console.log(writing);
+//         }
+//   });
 
 app.get("/", (req, res) => res.render("landing"));
 
 app.get("/writings", (req, res) => {
-  res.render("writings", {writings: writings});
+  Writing.find({}, (err, writings) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("writings", {writings: writings});
+    }
+  });
 });
 
 app.post("/writings", (req, res) => {
@@ -30,8 +50,13 @@ app.post("/writings", (req, res) => {
   let body = req.body.body;
   let newWriting = {title: title, body: body};
 
-  writings.push(newWriting);
-  res.redirect("/writings");
+  Writing.create(newWriting, (err, writing) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/writings");
+    }
+  });
 });
 
 app.get("/writings/new", (req, res) => {
