2b52177b4bdfdf248a2571bbc1dfcc58f133cf69
sola <solaocodes@gmail.com>
2019-02-20 23:16:43 -0500

    Add mongoDB development database.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').load();
+}
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.DEVELOPMENTDATABASEURL, { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
